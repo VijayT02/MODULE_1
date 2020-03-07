199d795bd0409ef28ea6624c6c19914073047233
VijayT02 <61898074+VijayT02@users.noreply.github.com>
2020-03-07 11:17:04 +0530

    Add files via upload

---
+++ pro1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+float r,a,c;
+printf("Enter the radius: \n");
+scanf("%f",&r);
+a=3.14*r*r;
+c=2*3.14*r;
+printf("Area of the circle = %f \n",a);
+printf("Circumference of the circle = %f \n",c);
+}
