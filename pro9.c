090bdc7fd924ad48738087155d7939e3508ccfee
VijayT02 <61898074+VijayT02@users.noreply.github.com>
2020-03-07 12:11:15 +0530

    Add files via upload

---
+++ pro9.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("Enter the value of a and b : \n");
+scanf("%d %d",&a,&b);
+printf("Before swapping \n");
+printf("a=%d  b=%d\n",a,b);
+c=a;
+a=b;
+b=c;
+printf("After swapping \n");
+printf("a=%d  b=%d \n",a,b);
+}
