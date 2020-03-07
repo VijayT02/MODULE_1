f92181ccc5f4b3fee1962d4475ac47274037e4c4
VijayT02 <61898074+VijayT02@users.noreply.github.com>
2020-03-07 11:31:40 +0530

    Add files via upload

---
+++ pro3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+float a,b,add,sub,multi,div;
+printf("Enter two numbers : \n");
+scanf("%f %f",&a,&b);
+add=a+b;
+sub=a-b;
+multi=a*b;
+div=a/b;
+printf("Addition of the two numbers = %f \n",add);
+printf("Subtraction of the two numbers = %f \n",sub);
+printf("Multiplication of the two numbers = %f \n",multi);
+printf("Division of the two numbers = %f \n",div);
+}
